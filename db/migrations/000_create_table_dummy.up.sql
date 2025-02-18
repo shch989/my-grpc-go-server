9bd8c1b0981449f3924b33e0e673ca3ad3471d74
shch989 <tankpop11@gmail.com>
2025-02-18 10:14:38 +0900

    Bank - Database Structure

--- db/migrations/000_create_table_dummy.up.sql
+++ db/migrations/000_create_table_dummy.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS dummy (
-  user_id UUID PRIMARY KEY,
-  user_name TEXT NOT NULL,
-  created_at TIMESTAMPTZ,
-  updated_at TIMESTAMPTZ
+  user_id       UUID PRIMARY KEY,
+  user_name     TEXT NOT NULL,
+  created_at    TIMESTAMPTZ,
+  updated_at    TIMESTAMPTZ
 );
